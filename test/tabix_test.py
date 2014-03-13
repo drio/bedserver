b16dcc319a433e2334599b3307b2d0b3d2ae8998
David Rio Deiros <driodeiros@gmail.com>
2014-03-13 15:29:22 -0400

    moving

---
+++ test/tabix_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+import pysam
+sys.path.append('..')
+
+
+from bedserver import compute
+
+
+if len(sys.argv) != 5:
+    print "./tabix_test.py sample.bed.gz chrm start stop"
+else:
+    fn, chrm, start, stop = sys.argv[1:]
+    dp = compute.data_points(fn, start, stop, chrm, 1, 44)
+    print len(dp)
+    print dp

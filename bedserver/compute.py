93a553ebef75b9fbaa5b84e5f32eee1d12ac1a04
David Rio Deiros <driodeiros@gmail.com>
2014-03-18 10:26:42 -0700

    Different implementation of logic to generate data points.

--- bedserver/compute.py
+++ bedserver/compute.py
@@ -5,22 +5,23 @@ def data_points(file_path, start, stop, chrm, step, size):
     start, stop, step, size = int(start), int(stop), int(step), int(size)
     points = []
     w_size = (stop-start)/size
+    print "compute(): w_size = %s" % w_size
 
-    p_window = start + w_size
-    w_vals = []
     tabixfile = pysam.Tabixfile(file_path)
-    for gtf in tabixfile.fetch(chrm, start, stop):
-        _chrm, _start, _end, _val = gtf.split()
-        _start, _end, _val = int(_start), int(_end), int(_val)
-
-        if _start < p_window:
+    i_start = start
+    for i in range(size):
+        w_vals = []
+        for gtf in tabixfile.fetch(chrm, i_start, i_start+w_size):
+            _chrm, _start, _end, _val = gtf.split()
+            _start, _end, _val = int(_start), int(_end), int(_val)
             w_vals.append(_val)
-        else:
+
+        if len(w_vals) > 0:
             points.append(sum(w_vals)/len(w_vals))
-            p_window = p_window + w_size
-            w_vals = [_val]
+        else:
+            points.append(0)
 
-    if len(w_vals) > 0:
-        points.append(sum(w_vals)/len(w_vals))
+        i_start = i_start + w_size
 
+    print "compute(): len(points) = %s" % len(points)
     return points[0:size]
